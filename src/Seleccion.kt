b206a9d4846acb99598e72cc223262d2878cdf5e
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-06-04 10:24:50 +0200

    "Creamos la subclass "Xogador" "

--- src/Seleccion.kt
+++ src/Seleccion.kt
@@ -4,6 +4,8 @@ Pasamos el Boletin 13 de progra (Java) a Kotlin
 /*
 Si inicializamos un parámetro de la siguiente manera, será como el valor por defecto y no haría falta ponerlo
 en el constructor, como un builder. Los parámetros por defecto se suelen poner al final
+Poniéndolos de esta forma, lo que esté entre paréntesis será los parámetros que recibe el constructor y el constructor
+en sí, además de los atributos de la clase
  */
 open class Seleccion ( var nombre:String, var apellido:String, var id:Int=0, var edad:Int=0 ) {
 
---
+++ src/Xogador.kt
@@ -0,0 +1,4 @@
+class Xogador (var dorsal:Int, var demarcacion:String, nombre:String,apellido:String,id:Int,edad:Int) : Seleccion(nombre,apellido,id,edad) {
+
+
+}
