59ccf4f536b032a14022780332b9016531331381
lvaqueiroperez <lvaqueiroperez@nautilus27.danielcastelao.org>
2019-06-04 09:51:52 +0200

    Initial commit

---
+++ src/Seleccion.kt
@@ -0,0 +1,12 @@
+/*
+Pasamos el Boletin 13 de progra (Java) a Kotlin
+ */
+/*
+Si inicializamos un parámetro de la siguiente manera, será como el valor por defecto y no haría falta ponerlo
+en el constructor, como un builder. Los parámetros por defecto se suelen poner al final
+ */
+open class Seleccion ( var nombre:String, var apellido:String, var id:Int=0, var edad:Int=0 ) {
+
+    
+
+}
