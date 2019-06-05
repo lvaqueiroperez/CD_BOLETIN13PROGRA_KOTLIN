63a507c283f2f9c917b4a29d68532944e9492fba
lvaqueiroperez <lvaqueiroperez@danielcastelao.org>
2019-06-05 20:54:13 +0200

    "Modificación clase Xogador"

--- src/Xogador.kt
+++ src/Xogador.kt
@@ -1,4 +1,9 @@
-class Xogador (var dorsal:Int, var demarcacion:String, nombre:String,apellido:String,id:Int,edad:Int) : Seleccion(nombre,apellido,id,edad) {
+class Xogador (  var dorsal:Int,  var emarcacion:String, nombre:String, apellido:String, id:Int, edad:Int)
+    : Seleccion(nombre,apellido,id,edad){
 
+    
 
 }
+
+
+
